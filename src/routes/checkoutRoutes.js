7070752d59a34e23d58771b6d6a9158837c3fb9d
Thinh Do <thinh.do@successsoftware.global>
2023-03-24 11:03:51 +0700

    1

--- src/routes/checkoutRoutes.js
+++ src/routes/checkoutRoutes.js
@@ -5,7 +5,6 @@ const checkoutRouters = express.Router();
 
 checkoutRouters.get("/checkout/price", auth, async (req, res) => {
   try {
-    
   } catch (error) {
     return res.status(200).send({
       error: error.message,
